8264e55c387ec8a798bcd429546ca0f34136bb71
marcio <marcio.weck@gmail.com>
2017-02-11 23:24:47 -0200

    Fiz algumas alterações para tentar mostrar os eixos, mas ainda não aparece, mas o tamanho e dimensões está correto.

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib import cm
-
+import matplotlib.ticker as ticker
 import numpy as np
 
 
@@ -18,35 +18,24 @@ def parse_area(area_str):
     
     return (a, b)
 
-    # fig1 = plt.figure()
-    # ax1 = fig1.add_subplot(111, aspect='equal')
-    # ax1.add_patch(
-    #     patches.Rectangle(
-    #         (0, 0),   # (x,y)
-    #         a,          # width
-    #         b,          # height
-    #         fill=False
-    #     )
-    # )
-    # plt.show()
-
-def draw_rect(ax, pos, a, b, fill=False, color='none'):
+def draw_rect(ax, pos, a, b, fill=False, edge_color='none', bg_color='none'):
     ax.add_patch(
         patches.Rectangle(
             pos,        # (x,y)
             a,          # width
             b,          # height
-            fill=False,
-            edgecolor=color
+            fill=fill,
+            facecolor=bg_color,
+            edgecolor=edge_color
         )
     )
 
     return ax
 
-def draw_triangle(ax, pos, b, h, fill=False, color='none'):
-    ax.plot([pos[0], pos[0]], [pos[1], pos[1]+b], c='b')
-    ax.plot([pos[0], pos[0]+h], [pos[1], pos[1]+b*0.5], 'b')
-    ax.plot([pos[0]+h, pos[0]], [pos[1]+b*0.5, pos[1]], 'b')
+def draw_triangle(ax, pos, b, h, fill=False, edge_color='none'):
+    ax.plot([pos[0], pos[0]+b], [pos[1], pos[1]], c=edge_color)
+    ax.plot([pos[0], pos[0]+b*0.5], [pos[1], pos[1]-h], c=edge_color)
+    ax.plot([pos[0]+b*0.5, pos[0]+b], [pos[1]-h, pos[1]], c=edge_color)
     return ax
 
 
@@ -64,9 +53,9 @@ def parse_file(ax, filename, dimensions):
 
         print(shape, size, pos)
         if shape == 'square':
-            return draw_rect(ax, (x,y), a, b, color='blue')
+            return draw_rect(ax, (x,y), a, b, edge_color='blue')
         elif shape == 'triangle':
-            return draw_triangle(ax, (x,y), a, b, color='blue')
+            return draw_triangle(ax, (x,y), a, b, edge_color='blue')
         else:
             print('Formato não encontrado.')
         
@@ -81,16 +70,25 @@ def plot_shape(filename, area="40x40"):
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111, aspect='equal')
 
-    ax2 = draw_rect(ax1, (0,0), a/float(a),a/float(a))
-
-    ax3 = parse_file(ax2, filename, (a,b))
-                                      
-    Y, X = np.mgrid[0:1:a/100., 0:1:b/100.]
+    xticks = np.arange(0, 1, 1./a)
+    yticks = np.arange(0, 1, 1./b)
     
-    plt.scatter(X, Y, c='k', alpha=0.7)
+    ax2 = draw_rect(ax1, (0,0), a/float(a),b/float(b), fill=True, bg_color='black')
+
+    ax3 = parse_file(ax1, filename, (a,b))
+    
+    ax3.xaxis.set_major_locator(ticker.MultipleLocator(1./a))
+    ax3.yaxis.set_major_locator(ticker.MultipleLocator(1./b))
+
+    ax3.xaxis.set_ticklabels(xticks*a) 
+    ax3.yaxis.set_ticklabels(yticks*b)
+    ax3.xaxis.tick_top()
+    ax3.invert_yaxis()    
+
 
 if __name__ == "__main__":
     import argparse
+    import os
 
     parser = argparse.ArgumentParser(description='Desenha arquivos da camada II-III do NeuronalSys.')
     parser.add_argument('-a', '--area', type=str, default="40x40",
@@ -99,6 +97,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    plot_shape(args.file, args.area)
+    _, filename = os.path.split(r""+args.file)
+
+    plot_shape(filename, args.area)
     plt.show()
 
